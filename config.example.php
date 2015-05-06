05cb0bf3de84d7899af0ae5b0645ea443e9f83a2
CHEN Mingyi <imcmy@outlook.com>
2015-05-06 11:11:57 +0800

    Add Config Example
    Add Config Example

---
+++ config.example.php
@@ -0,0 +1,34 @@
+<?php
+
+class Config
+{
+	// DataBase
+	public static $DB_User = '';
+	public static $DB_Pass = '';
+	public static $DB_host = '';
+	public static $DB_port = '';
+
+	// Blog
+	public static $DB_Blog_User = '';
+	public static $DB_Blog_Pass = '';
+
+	// CMS
+	public static $DB_CMS_User = '';
+	public static $DB_CMS_Pass = '';
+	
+	// PublicService
+	public static $DB_PS_User = '';
+	public static $DB_PS_Pass = '';
+
+	// TrainPlatform
+	public static $DB_TP_User = '';
+	public static $DB_TP_Pass = '';
+	public static $TP_URL = '';
+
+	// UserCenter
+	public static $DB_UC_User = '';
+	public static $DB_UC_Pass = '';
+
+	//Log
+	public static $Log_URL = '';
+}
